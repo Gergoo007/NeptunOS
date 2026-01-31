6747b5a4c7e066a43d66d259c257b1bb67dd4f92
Gergoo007 <h.gergoo007@gmail.com>
2026-01-31 14:11:37 +0100

    Small fixes for portability

--- kernel/amd64.mk
+++ kernel/amd64.mk
@@ -10,14 +10,24 @@ CXXFLAGS += -mgeneral-regs-only -ffreestanding -nostdlib -nostdinc \
 	-Isrc -xc++ -std=gnu++2c -Wall -Wextra -Wshadow -Wno-address-of-packed-member \
 	-Wno-unused-parameter -fno-stack-protector -mno-red-zone -O0 -g -Wno-multichar \
 	-Wno-packed-bitfield-compat -msse2 -fno-omit-frame-pointer -fno-rtti -fno-exceptions \
-	-MMD -MP -DDEBUG -fno-lto -fstack-protector-strong -Wno-format -Wno-implicit-fallthrough
+	-MMD -MP -DDEBUG -fno-lto -fstack-protector-strong -Wno-format -Wno-implicit-fallthrough -fno-PIC
 ASFLAGS += -fno-lto
 LDFLAGS += --no-gc-sections
 
+ifneq (,$(shell command -v x86_64-elf-g++ 2>/dev/null))
+CC := g++
+LD ?= ld
+AS ?= gcc
+STRIP ?= strip
+
+ESC = $(shell printf '\033')
+$(info $(ESC)[1m$(ESC)[38;5;9mNative toolchain detected! If the kernel doesn't work, install x86_64-elf-*!$(ESC)[0m)CXXFLAGS += -fno-stack-protector
+else
 CC := x86_64-elf-g++
 LD ?= x86_64-elf-ld
 AS ?= x86_64-elf-gcc
 STRIP ?= x86_64-elf-strip
+endif
 
 # CC := clang++ -fno-lto
 # LD ?= ld.lld -m elf_x86_64
